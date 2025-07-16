15f06cd4bc0eb97b108d973baec082e432c68a6b
Seobkim-Desktop-Win <wnstjqaodls@gmail.com>
2025-07-16 21:17:37 +0900

    :wrench: 문서자동화 Swagger 자바 Config 설정추가

---
+++ src/main/java/kr/hhplus/be/server/config/SwaggerConfiguration.java
@@ -0,0 +1,24 @@
+package kr.hhplus.be.server.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+            .title("이커머스 API 명세")
+                .version("v0.1")
+                .description("이커머스 API 에 대한 Mock API 명세내용들입니다.")
+            );
+
+    }
+
+}
