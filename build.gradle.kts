0abbae61ec8c7662c83772a17a82b9d3daedf55d
Seobkim-Desktop-Win <wnstjqaodls@gmail.com>
2025-07-16 21:16:51 +0900

    :heavy_plus_sign: 문서 자동화 Swagger 의존성추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
