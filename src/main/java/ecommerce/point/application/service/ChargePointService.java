4ee2ebf936c0ca41d11bf444c18baf27067d8d58
Seobkim-Desktop-Win <wnstjqaodls@gmail.com>
2025-07-25 04:04:54 +0900

    feat: 포인트 - 포인트충전서비스 개발

--- src/main/java/ecommerce/point/application/service/ChargePointService.java
+++ src/main/java/ecommerce/point/application/service/ChargePointService.java
@@ -1,8 +1,36 @@
 package ecommerce.point.application.service;
 
+import ecommerce.point.application.port.in.ChargePointUseCase;
+import ecommerce.point.application.port.out.LoadPointPort;
+import ecommerce.point.application.port.out.SavePointPort;
 import ecommerce.point.domain.Activity;
 import ecommerce.point.domain.ActivityWindow;
+import ecommerce.point.domain.Point;
 
-public class ChargePointService {
+public class ChargePointService implements ChargePointUseCase {
 
+    private final LoadPointPort loadPointPort;
+    private final SavePointPort savePointPort;
+
+
+    public ChargePointService(ActivityWindow activityWindow, LoadPointPort loadPointPort, SavePointPort savePointPort) {
+        this.loadPointPort = loadPointPort;
+        this.savePointPort = savePointPort;
+    }
+
+    public long charge(Long pointId, long amount) {
+
+        ActivityWindow activityWindow = loadPointPort.loadActivityWindow(pointId);
+
+        // 도메인 로직 실행
+        Point point = new Point(pointId);
+        Activity newActivity = point.charge(activityWindow, amount);
+
+        // 새로운 Activity를 저장
+        savePointPort.saveActivity(newActivity);
+
+        // 새로운 잔액 반환
+        long newBalance = activityWindow.calculateBalance() + amount;
+        return newBalance;
+    }
 }
