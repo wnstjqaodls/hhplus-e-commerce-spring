9ae5e2df73ef469ce084b68ff21a5701e6fef8d0
Seobkim-Desktop-Win <wnstjqaodls@gmail.com>
2025-08-14 02:49:45 +0900

    fix: 포인트 - 게터 생성, 세터제거

--- src/main/java/ecommerce/point/domain/Point.java
+++ src/main/java/ecommerce/point/domain/Point.java
@@ -3,6 +3,23 @@ package ecommerce.point.domain;
 public class Point {
     private static final long CHARGE_LIMIT = 1_000_000L;
 
+    private final Long id;
+
+    private long amount;
+
+    public Point () {
+        this.id = null; // 초기화 시 ID는 null로 설정
+        this.amount = 0; // 초기 잔액은 0으로 설정
+    }
+    public Point (Long id) {
+        this.id = id;
+    }
+
+    public Point (Long id, long amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
     public long getAmount () {
         return amount;
     }
@@ -11,30 +28,17 @@ public class Point {
         this.amount = amount;
     }
 
-    public Point () {
-        this.id = null; // 초기화 시 ID는 null로 설정
-        this.amount = 0; // 초기 잔액은 0으로 설정
+    public Long getId () {
+        return id;
     }
 
-    private final Long id;
-
-    private long amount;
-
-    public Point(Long id) {
-        this.id = id;
-    }
-    public Point(Long id, long amount) {
-        this.id = id;
-        this.amount = amount;
-    }
-
-    public void charge(long amount) {
+    public void charge (long amount) {
         validateChargeAmount(amount);
         this.amount += amount;
 
     }
 
-    public void use(long amount, Long useAmount) {
+    public void use (long amount, Long useAmount) {
         long currentBalance = calculateBalance();
         validateUseAmount(currentBalance, amount);
     }
@@ -43,20 +47,17 @@ public class Point {
         return getAmount();
     }
 
-    private void validateChargeAmount(long amount) {
+    private void validateChargeAmount (long amount) {
         if (amount > CHARGE_LIMIT) {
             throw new IllegalArgumentException("충전 한도를 초과했습니다.");
         }
     }
 
-    private void validateUseAmount(long currentBalance, long amount) {
+    private void validateUseAmount (long currentBalance, long amount) {
         if (currentBalance < amount) {
             throw new IllegalArgumentException("잔액이 부족합니다.");
         }
     }
 
-    public Long getId() {
-        return id;
-    }
 
 }
