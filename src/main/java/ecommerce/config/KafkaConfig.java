88939a31301d40a8d461523b02aa213f56bc4986
Seobkim-Desktop-Win <wnstjqaodls@gmail.com>
2025-09-05 01:42:35 +0900

    feat: 설정파일추가 - 카프카 설정 추가

---
+++ src/main/java/ecommerce/config/KafkaConfig.java
@@ -0,0 +1,48 @@
+package ecommerce.config;
+
+import com.fasterxml.jackson.databind.JsonSerializer;
+import ecommerce.common.dto.MessageDto;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    // 토픽 컨벤션 : 도메인단어-행위
+    // ex) order-created, coupon-issued, product-updated
+    public static final String COUPON_ISSUE_REQUEST_TOPIC = "coupon-issue-request";
+    public static final String ORDER_COMPLETED_TOPIC = "order-completed";
+
+    @Bean
+    public ProducerFactory<String, MessageDto> producerFactory() {
+        Map<String,Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        // 메시지 key 를 문자열로 변환하는 방법
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        // 메시지 value 를 JSON 으로 변환하는 방법
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, MessageDto> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+
+
+
+
+
+
+}
